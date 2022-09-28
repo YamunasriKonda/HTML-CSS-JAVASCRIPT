8ad54634f8c786ff1ed45ed77ea8fa6e330b307c
yamunasrikonda <yamuna.jakkam91@gmail.com>
2022-09-28 15:35:21 +0100

    pp

---
+++ JS script calculator.js
@@ -0,0 +1,35 @@
+//let a=0;
+//let b=0;
+//let op=0;
+
+
+function calc(a,b,op) {
+
+    switch (op) {
+        case "+":
+
+        console.log( a+b)
+
+        break;
+
+        case"-":
+        console.log( a-b)
+
+        break;
+
+        case "*":
+
+            console.log( a*b)
+
+        break;
+
+        case "%":
+
+            console.log( a%b)
+
+        break;
+
+    }
+}
+
+calc(26,36,"*");
